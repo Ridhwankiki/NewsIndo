636633afc9382550b1a4746bef5db82e1dc0dda2
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-23 11:16:15 +0700

    Safari Services

--- NewsIndo/View/ContentView.swift
+++ NewsIndo/View/ContentView.swift
@@ -6,19 +6,86 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct ContentView: View {
+    
+    @StateObject private var newsVM = NewsVM()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(newsVM.articles) { article in
+                    VStack (alignment: .leading, spacing: 16) {
+                        AsyncImage(url: URL(string: article.image.medium)) {
+                            image in
+                            image.resizable()
+                                .scaledToFit().clipShape(RoundedRectangle(cornerRadius: 10))
+                        } placeholder: {
+                            ZStack {
+//                                Color.red
+                                waitingView()
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+                        Text(article.title)
+                            .font(.headline)
+                        
+                        Text(article.author)
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        
+                        HStack {
+                            Text(article.isoDate.relativeToCurrentDate())
+                                .font(.subheadline)
+                            
+                            Button {
+                                let vc = SFSafariViewController(url: URL(string: article.link)!)
+                                UIApplication.shared.firstKeyWindow?.rootViewController?.present(vc, animated: false)
+                            } label: {
+                                Text("Selengkapnya")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.purple)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle(Constant.newsTitle)
+            .task {
+                await newsVM.fetchNews()
+            }
+            .overlay(newsVM.isLoading ? ProgressView() : nil)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+@ViewBuilder
+func waitingView() -> some View {
+    VStack(spacing : 20) {
+        ProgressView()
+            .progressViewStyle(.circular)
+            .tint(.pink)
+        
+//        Text("Fetch Image....")
+    }
+}
+
+extension UIApplication {
+    var firstKeyWindow: UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .compactMap { scene in
+                scene as? UIWindowScene
+            }
+            .filter { filter in
+                filter.activationState == .foregroundActive
+            }
+            .first?.keyWindow
+    }
 }
---
+++ NewsIndo/ViewModel/NewsVM.swift
@@ -0,0 +1,31 @@
+//
+//  NewsVM.swift
+//  NewsIndo
+//
+//  Created by MacBook Pro on 23/04/24.
+//
+
+import Foundation
+
+@MainActor
+class NewsVM: ObservableObject {
+    @Published var articles = [NewsArticle]()
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    func fetchNews() async {
+        
+        isLoading = true
+        errorMessage = nil
+        defer { isLoading = false}
+        
+        do {
+            articles = try await APIService.shared.fetchNews()
+//            isLoading = false
+        } catch {
+            errorMessage = "🔥 \(error.localizedDescription). Failed to fetch News from API!!! 🔥"
+            print(errorMessage ?? "N/A")
+//            isLoading = false
+        }
+    }
+}
