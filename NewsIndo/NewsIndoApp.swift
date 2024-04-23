18c871c7ba863040a094c8646c8bb25ebc7962cd
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-23 08:47:42 +0700

    Initial Commit

---
+++ NewsIndo/NewsIndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsIndoApp.swift
+//  NewsIndo
+//
+//  Created by MacBook Pro on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsIndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
